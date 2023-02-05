9db9c88f26a5953efa74d841737f29f1b58410d1
Laiba111 <114950237+Laiba111@users.noreply.github.com>
2023-02-05 18:25:09 +0500

    Trip History Tables

---
+++ backend/models/triphistory/driverTrips.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const DriverTripsSchema = new mongoose.Schema({
+  source: {
+    type: String,
+    required: true,
+  },
+  destination: {
+    trpe: String,
+    required: true,
+  },
+  fare: {
+    type: Number,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  numOfPassengers: {
+    type: Number,
+    required: true,
+  },
+  driverID: {
+    //foreign
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  vehicleID: {
+    //foreign
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.export("DriverTrips", DriverTripsSchema);
---
+++ backend/models/triphistory/passengerTrips.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const PassengerTripsSchema = new mongoose.Schema({
+  source: {
+    type: String,
+    required: true,
+  },
+  destination: {
+    trpe: String,
+    required: true,
+  },
+  fare: {
+    type: Number,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  tripID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "DriverTrips",
+  },
+  userID: {
+    //foreign
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.export("PassengerTrips", PassengerTripsSchema);
