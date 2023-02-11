410ff451ec4a718e7cfd37d3050240291ac87a73
AbdullahUmer007 <120726391+AbdullahUmer007@users.noreply.github.com>
2023-02-11 18:15:04 +0500

    Create driverMiddleware.js

---
+++ backend/middlewares/driver-auth/driverMiddleware.js
@@ -0,0 +1,27 @@
+const User = require("../../models/users/userModel");
+const Driver = require("../../models/users/driverModel");
+
+exports.isDriver = async (req, res, next) => {
+  const { userName } = req.body;
+  var user = await User.findOne({
+    userName: userName,
+  });
+  if (!user) {
+    return res.status(403).json({
+      success: false,
+      code: -2,
+      message: "User does not exist.",
+    });
+  }
+  var driver = await Driver.findOne({
+    userID: user._id,
+  });
+  if (!driver) {
+    return res.status(403).json({
+      success: false,
+      code: -2,
+      message: "Driver does not exist.",
+    });
+  }
+  next();
+};
