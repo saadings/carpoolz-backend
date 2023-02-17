175f3a7ec21e95e7245a4788de0d65bfedb213c1
AbdullahUmer007 <120726391+AbdullahUmer007@users.noreply.github.com>
2023-02-17 23:00:08 +0500

    updated driver middleware

--- backend/middlewares/driver-auth/driverMiddleware.js
+++ backend/middlewares/driver-auth/driverMiddleware.js
@@ -1,6 +1,5 @@
 const User = require("../../models/users/userModel");
 const Driver = require("../../models/users/driverModel");
-const UserSession = require("../../models/user-session/userSessionModel");
 
 exports.isDriver = async (req, res, next) => {
   const { userName } = req.body;
