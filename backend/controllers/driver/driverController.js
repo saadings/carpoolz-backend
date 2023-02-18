65b5ffca28f1e9afe24f90ae7b3c56caeb61dbc2
AbdullahUmer007 <120726391+AbdullahUmer007@users.noreply.github.com>
2023-02-18 16:26:38 +0500

    Vehicle api

--- backend/controllers/driver/driverController.js
+++ backend/controllers/driver/driverController.js
@@ -1,6 +1,7 @@
 var User = require("../../models/users/userModel");
 var User = require("../../models/users/userModel");
 var Driver = require("../../models/users/driverModel");
+var Vehicle = require("../../models/vehicle/vehicleModel");
 
 exports.registerDriver = async (req, res) => {
   const { userName, cnic, licenseNo } = req.body;
@@ -20,7 +21,7 @@ exports.registerDriver = async (req, res) => {
       return res.status(403).json({
         success: false,
         code: -2,
-        message: "User does not exist.",
+        message: "User does not registered.",
       });
 
     var driver = await Driver.findOne({
@@ -31,7 +32,7 @@ exports.registerDriver = async (req, res) => {
       return res.status(403).json({
         success: false,
         code: -3,
-        message: "Driver already exist.",
+        message: "Driver already registered.",
       });
 
     var newDriver = new Driver({
@@ -51,6 +52,88 @@ exports.registerDriver = async (req, res) => {
       success: true,
       code: 0,
       message: "Driver created successfully.",
+    });
+  } catch (error) {
+    return res.status(500).json(serverError());
+  }
+};
+
+exports.registerVehicle = async (req, res) => {
+  const {
+    userName,
+    vehicleType,
+    vehicleNumber,
+    vehicleModel,
+    vehicleColor,
+    numOfSeats,
+  } = req.body;
+  try {
+    if (
+      !userName ||
+      !vehicleType ||
+      !vehicleNumber ||
+      !vehicleModel ||
+      !vehicleColor ||
+      !numOfSeats
+    ) {
+      return res.status(400).json({
+        success: false,
+        code: -1,
+        message: "Please provide all the required fields.",
+      });
+    }
+
+    var user = await User.findOne({
+      userName: userName,
+    });
+    if (!user)
+      return res.status(403).json({
+        success: false,
+        code: -2,
+        message: "User does not registered.",
+      });
+
+    var driver = await Driver.findOne({
+      userID: user._id,
+    });
+
+    if (!driver)
+      return res.status(403).json({
+        success: false,
+        code: -3,
+        message: "Driver does not registered.",
+      });
+
+    var vehicle = await Vehicle.findOne({
+      vehicleNumber: vehicleNumber,
+    });
+
+    if (vehicle)
+      return res.status(403).json({
+        success: false,
+        code: -4,
+        message: "Vehicle already registered.",
+      });
+
+    var newVehicle = new Vehicle({
+      driverID: driver._id,
+      vehicleType: vehicleType,
+      vehicleNumber: vehicleNumber,
+      vehicleModel: vehicleModel,
+      vehicleColor: vehicleColor,
+      numOfSeats: numOfSeats,
+    });
+
+    try {
+      await newVehicle.save();
+    } catch (error) {
+      return res.status(400).json(validationError(error));
+    }
+
+    return res.status(201).json({
+      success: true,
+      code: 0,
+      message: "Vehicle registered successfully.",
     });
   } catch (error) {
     return res.status(500).json(serverError());
--- backend/routes/driver.js
+++ backend/routes/driver.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 const driverController = require("../controllers/driver/driverController");
 const { isDriver } = require("../middlewares/driver-auth/driverMiddleware");
-const {
-  isJWTValid,
-} = require("../middlewares/jwt/jwtMiddleware");
+const { isJWTValid } = require("../middlewares/jwt/jwtMiddleware");
 /* GET users listing. */
 router.post("/register", isJWTValid, isDriver, driverController.registerDriver);
+router.post("/register/vehicle", isJWTValid, driverController.registerVehicle);
 module.exports = router;
